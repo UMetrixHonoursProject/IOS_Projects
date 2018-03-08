89e013e9d387860dd09f259f9cf57fd1ce6773b3
RamyaKeerthana <keerthana.a@students.iiit.ac.in>
2018-03-09 03:34:54 +0530

    Show/Hide Password Code Snippet and Source Code

---
+++ Password_Hide_Show/Password_Hide_Show/Password_Hide_Show/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Password_Hide_Show
+//
+//  Created by serc student on 3/5/18.
+//  Copyright © 2018 ramyakeerthana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Password_Hide_Show/Password_Hide_Show/Password_Hide_Show/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Password_Hide_Show
+//
+//  Created by serc student on 3/5/18.
+//  Copyright © 2018 ramyakeerthana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *password;
+
+@end
+
