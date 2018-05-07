11b7040be5c8184a915284bd841475f24eead006
RamyaKeerthana <keerthana.a@students.iiit.ac.in>
2018-05-07 14:13:36 +0530

    First-time user has multiple sign-up options

---
+++ Login/Login/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by serc student on 2/4/18.
+//  Copyright © 2018 ramyakeerthana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var passwd: UITextField!
+    
+    
+    @IBAction func greeno(sender: AnyObject) {
+        passwd.secureTextEntry=false
+    }
+
+    @IBAction func greeonin(sender: AnyObject) {
+        passwd.secureTextEntry=true
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // for google login
+    // https://developers.google.com/identity/sign-in/ios/start-integrating
+
+
+}
+
